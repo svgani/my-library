cbbde4ea1994050dade7c5ad5042528cff908e2f
Gani prabha <prabha.chekkapalli@gmail.com>
2023-03-06 15:46:10 +0530

    logic

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-export class calc {
-  add = (a, b) => a + b;
-  subtract = (a, b) => a - b;
-  multiply = (a, b) => a * b;
-  divide = (a, b) => a * b;
-}
+const add = (...numbers) => numbers.reduce((total, num) => total + num);
+const subtract = (a, b) => a - b;
+const multiply = (...numbers) => numbers.reduce((total, num) => total * num);
+const divide = (a, b) => a * b;
+
+export {
+  add,
+  subtract,
+  multiply,
+  divide
+};
